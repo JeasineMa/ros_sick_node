7c2ffcf599fd3380c2a6877d6ba55b3316d8659c
jeasinema <jeasinema@gmail.com>
2016-05-18 01:30:01 +0800

    first commit

---
+++ build/sick_tim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jeasinema/Documents/catkin_workspace/src/sick_tim
+# Build directory: /home/jeasinema/Documents/catkin_workspace/build/sick_tim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
